756b78e27d2130d50f7bd2dcdd96158a4124a714
Boris Nagaev <bnagaev@gmail.com>
2015-08-01 20:48:21 +0300

    main: no try-except
    It was external try-catch and keeps working.

--- main.py
+++ main.py
@@ -14,23 +14,20 @@ global wsk
 
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(self):
-        try:
-            """Respond to a GET request."""
-            path, query = self.path.split('?', 1)
-            qs = urlparse.parse_qs(query)
-            login = qs['login'][0]
-            task = qs['task'][0]
-            rating1 = qs['rating'][0]
-            password = qs['password'][0]
-            assert password == config.password # protection
-            print('Rating %s %s %s' % (login, task, rating1))
-            rating.update_rating(wks, login, task, rating1)
-            print('Rating updated')
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            self.wfile.write("Ok")
-        except Exception, e:
-            print(e)
+        """Respond to a GET request."""
+        path, query = self.path.split('?', 1)
+        qs = urlparse.parse_qs(query)
+        login = qs['login'][0]
+        task = qs['task'][0]
+        rating1 = qs['rating'][0]
+        password = qs['password'][0]
+        assert password == config.password # protection
+        print('Rating %s %s %s' % (login, task, rating1))
+        rating.update_rating(wks, login, task, rating1)
+        print('Rating updated')
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write("Ok")
 
 if __name__ == '__main__':
     wks = worksheet.get_worksheet()
