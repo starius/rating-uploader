983a0d306ec20d930008e867db765b245e52c410
Boris Nagaev <bnagaev@gmail.com>
2015-08-04 22:21:46 +0300

    start impementing deadlines

--- rating.py
+++ rating.py
@@ -1,5 +1,7 @@
-import config
 from repoze.lru import lru_cache
+import datetime
+
+import config
 
 @lru_cache(maxsize=500)
 def get_logins(worksheet):
@@ -24,7 +26,15 @@ def set_rating(worksheet, login, task, rating):
     i, j = get_cell_indexes(worksheet, login, task)
     worksheet.update_cell(i, j, rating)
 
+def get_deadline(worksheet, login, task):
+    return '2099-01-01' # TODO
+
+def now():
+    return datetime.date.today().strftime("%Y-%m-%d")
+
 def update_rating(worksheet, login, task, rating):
+    if now() > get_deadline(worksheet, login, task):
+        return
     prev = -1
     try:
         prev = float(get_rating(worksheet, login, task))
